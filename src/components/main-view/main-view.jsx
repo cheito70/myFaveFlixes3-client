112e7ea7845e55c3e4de0cb2dca40fd19f3f932e
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-11-19 01:24:39 -0600

    Added import col

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,7 +6,7 @@ import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import Row from 'react-bootstrap/Row';
-
+import Col from 'react-bootstrap/Col';
 
 export class MainView extends React.Component {
 
