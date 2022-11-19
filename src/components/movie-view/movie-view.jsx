7140b9c75341e57786678bdb7ecc36cfc81e22f0
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-11-19 01:08:37 -0600

    Added Cols and Rows

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 export class MovieView extends React.Component {
 
@@ -14,29 +16,50 @@ export class MovieView extends React.Component {
     render() {
         const { movie, onBackClick } = this.props;
 
-        return (
+        return (            
             <div className="movie-view">
+                <Row>
+                    <Col>
                 <div className="movie-poster">
                     <img crossorigin="anonymous" src={movie.ImagePath} />
                 </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                 <div className="movie-title">
                     <span className="label">Title: </span>
                     <span className="value">{movie.Title}</span>
                 </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                 <div className="movie-description">
                     <span className="label">Description: </span>
                     <span className="value">{movie.Description}</span>
                 </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                 <div className="movie-genre">
                     <span className="label">Genre: </span>
                     <span className="value">{movie.Genre.Name}</span>
                 </div>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                 <div className="movie-director">
                     <span className="label">Director: </span>
                     <span className="value">{movie.Director.Name}</span>
-                </div>   
+                </div>
+                    </Col>
+                </Row>
+                <Col>
                 <button onClick={() => { onBackClick(null); }}>Back</button>        
-            
+                </Col>
             </div>
         );
     }
