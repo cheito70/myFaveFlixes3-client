9b4400211d65fede6dcfdf3ca63319151dd6d37a
cheito70 <49110712+cheito70@users.noreply.github.com>
2022-11-19 00:13:18 -0600

    Added bootstrap code to return on movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,7 +7,17 @@ export class MovieCard extends React.Component {
     render() {
         const { movie, onMovieClick } = this.props;
 
-        return <div className="movie-card" onClick={() => onMovieClick(movie)}>{movie.Title}</div>;
+        //return <div className="movie-card" onClick={() => onMovieClick(movie)}>{movie.Title}</div>;
+        return (
+            <Card>
+                <Card.Img variant="top" src={movie.ImagePath} />
+                <Card.Body>
+                    <Card.Title>{movie.Title}</Card.Title>
+                    <Card.Text>{movie.Description}</Card.Text>
+                    <Button onClick={() => onMovieClick(movie)} vaiant="link">Open</Button>
+                </Card.Body>
+            </Card>
+        );
     }
 }
 
